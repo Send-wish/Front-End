956fecf5c2aae658d8d95fbc0eb42d80d2081254
giyoun <109953972+Gi-Youn-Oh@users.noreply.github.com>
2023-01-09 16:43:50 +0900

    함수 정리

--- src/components/Collection/Patch.js
+++ src/components/Collection/Patch.js
@@ -1,27 +1,37 @@
 
-const _changeCollectionName = async () => {
-    setVisibleModal(false)
-    try{
-    await fetch('http://localhost:8080/collection', {
-    method: 'PATCH',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-        nickname: nickName,
-        collectionId: collectionId,
-        newTitle: ChangedColName,
-    })
-    })
-    .then(response => {
-        if (!response.ok) {
-          throw new Error(`${response.status} 에러발생`);
-        }
-        return response.json();
+  const _changeCollectionName = async () => {
+    setVisibleModal(false);
+    console.log('data check', nickName, collectionId, collectionName)
+    try {
+      await fetch('https://api.sendwish.link:8081/collection', {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          nickname: nickName,
+          collectionId: collectionId,
+          newTitle: collectionTitle,
+        }),
       })
-    .then(result => {console.log('result', result)}) //for debug
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`${response.status} 에러발생`);
+          }
+          return response.json();
+        })
+        .then(data => {
+          console.log(data)
+
+          setIsChanged(true)
+
+          console.log("chagned true?", isChanged)
+          console.log('change_check!!',collectionTitle)
+        })
+        .then(result => {
+          console.log('result', result);
+        }); //for debug
     } catch (e) {
-    console.log('change fail');
-  }
-};
-    
+      console.log('change fail', e);
+    }
+  };
---
+++ src/components/Main/MakeCollection.js
@@ -0,0 +1,54 @@
+const _madeCollection = async () => {
+    console.log('nickName from Sign In', nickName); // 로그인 화면에서 받아온 닉네임 확인
+    console.log('collectionName', collectionName); // 컬렉션 이름 확인
+
+    setVisibleModal(false); 
+    
+    try {
+      fetch('https://api.sendwish.link:8081/collection', {
+        method: 'POST',
+        headers: {'Content-Type': `application/json`},
+        body: JSON.stringify({
+          nickname: nickName,
+          title: collectionName,
+        }),
+      })
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`${response.status} 에러발생`);
+          }
+          return response.json();
+        })
+        .then(json => console.log(json))
+        .then(data => {
+          console.log('made collection', data);
+        })
+        .then(() => _getCollections());
+    } catch (e) {
+      console.log('collection made fail');
+    }
+  };
+
+  const _getCollections = async () => {
+    setLoading(true);
+    try {
+      fetch(`https://api.sendwish.link:8081/collections/${nickName}`, {
+        method: 'GET',
+        // headers: {Content_Type: 'application/json'},
+      })
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          setCollections(data);
+          console.log('get collections', data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
