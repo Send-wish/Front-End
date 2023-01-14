d242ff7d69389cbdd9cc6e5514345e303d36a361
giyoun <109953972+Gi-Youn-Oh@users.noreply.github.com>
2023-01-14 19:35:12 +0900

    chat navigate button

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import {
   SignIn,
   SignUp,
   SharedCollection,
+  ChatRoom,
 } from './src/screens';
 import Share from './Share';
 
@@ -104,8 +105,7 @@ const App = () => {
           <Stack.Screen name="Collection" component={Collection} />
           <Stack.Screen name="SharedCollection" component={SharedCollection} />
           <Stack.Screen name="Share" component={Share} />
-
-          {/* <Stack.Screen name="ChatRoom" component={ChatRoom} /> */}
+          <Stack.Screen name="ChatRoom" component={ChatRoom} />
         </Stack.Navigator>
       </NavigationContainer>
     </ThemeProvider>
---
+++ src/screens/Chatroom.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {View, Text} from "react-native";
+
+const ChatRoom = () => {
+    return (
+        <View>
+            <Text>Chatroom</Text>
+        </View>
+    );
+};
+
+
+export default ChatRoom;
