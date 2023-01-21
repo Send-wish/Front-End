57d23626fbd02abc6866b66e33ad496058613f09
brido4125 <hcs4125@gmail.com>
2023-01-21 16:09:27 +0900

    update:VideoSdk

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {register} from '@videosdk.live/react-native-sdk';
 
+register();
 AppRegistry.registerComponent(appName, () => App);
---
+++ ios/SandWish/VideosdkRPK.m
@@ -0,0 +1,15 @@
+//
+//  VideosdkRPK.m
+//  SandWish
+//
+//  Created by 홍창섭 on 2023/01/21.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import "React/RCTEventEmitter.h"
+
+@interface RCT_EXTERN_MODULE(VideosdkRPK, RCTEventEmitter)
+RCT_EXTERN_METHOD(startBroadcast)
+
+@end
